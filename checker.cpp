7e40b18744db689a51a1be7035769c17e7f9a1e9
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-03-07 19:38:34 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,60 @@
+#include "stdafx.h"
 #include <assert.h>
 #include <iostream>
+#include <string.h>
+#include <string>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+const float TEMP_MIN_THRESHOLD =0.00f;
+const float TEMP_MAX_THRESHOLD =45.00f;
+
+const float SOC_MIN_THRESHOLD = 1;
+const float SOC_MAX_THRESHOLD =100;
+
+const float CHARGE_RATE_MIN_THRESHOLD = 0.00f;
+const float CHARGE_RATE_MAX_THRESHOLD =0.80f;
+
+bool checkValueInRange(float min, float max, const float &valueToCheck, const std::string &paramName)
+{
+	if (valueToCheck >= min && valueToCheck <= max) return true;
+	else
+	{
+		std::cout << paramName << " out of range!" << endl;
+		return false;
+	}
+}
+
+bool TemperatureIsOk(const float temperature)
+{
+	return checkValueInRange(TEMP_MIN_THRESHOLD, TEMP_MAX_THRESHOLD, temperature, "Temperature");
+}
+
+bool SocIsOk(const float soc)
+{
+	return checkValueInRange(SOC_MIN_THRESHOLD, SOC_MAX_THRESHOLD, soc, "State of Charge");
+}
+
+bool ChargeRateIsOk(const float chargeRate)
+{
+	return checkValueInRange(CHARGE_RATE_MIN_THRESHOLD, CHARGE_RATE_MAX_THRESHOLD, chargeRate, "Charge Rate");
+}
+
+bool batteryIsOk(const float temperature, const  float soc, const  float chargeRate) {
+	bool temperatureOkIndicator, socOkIndicator, chargeRateOkIndicator = false;
+
+	temperatureOkIndicator = TemperatureIsOk(temperature);
+	socOkIndicator = SocIsOk(soc);
+	chargeRateOkIndicator = ChargeRateIsOk(chargeRate);
+
+	return (temperatureOkIndicator && socOkIndicator && chargeRateOkIndicator);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+	assert(batteryIsOk(25.0F, 70.0F, 0.7F) == true);  // all okay
+	assert(batteryIsOk(50.0F, 85.0F, 0.0F) == false); // temp and soc high
+	assert(batteryIsOk(50.0F, 65.0F, 0.0F) == false); // temp high
+	assert(batteryIsOk(-50.0F, 80.0F, 0.0F) == false); // temp low
+	assert(batteryIsOk(40.0F, 15.0F, 0.0F) == false); // soc low
+	assert(batteryIsOk(40.0F, 85.0F, 0.0F) == false); // soc high
+	assert(batteryIsOk(40.0F, 65.0F, 0.81F) == false); // charge rate high
 }
